0a04e0970d4063cacb8a03466b4a48c645c47b71
matias798 <matiquiro.mq@gmail.com>
2020-11-16 01:43:18 -0300

    el numero de puerto pasa a ser una constante y agregue un app.get

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-
+const PORT=3000;
 
 app.get('/',(req,res)=>{
-res.sendFile(__dirname + '/views/index.html' );
+
+res.sendFile(__dirname + '/view' + req.url );
 
 });
-app.listen(3001,()=>{console.log('estamos corriendo en el puerto 3001')});
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public' + req.url);
+});
+app.listen(3001,()=>{console.log('Estamos corriendo en el puerto'+ PORT)});
